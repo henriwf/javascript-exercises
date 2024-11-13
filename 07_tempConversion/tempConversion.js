0da48a7dc95657a6eb3dea235f8b0be0947e55d0
“henriwf” <henriwolfgangf@gmail.com>
2024-11-13 15:07:06 -0300

    -

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius  = function(tempF) {
+  let converted = (tempF - 32) * 5 / 9;
+  converted = Math.round(converted*10)/10;
+  return converted;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let converted = tempC * 1.8 + 32;
+  converted = Math.round(converted*10)/10;
+  return converted;
 };
+
 
 // Do not edit below this line
 module.exports = {
