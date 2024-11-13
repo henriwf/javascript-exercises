6088f7da3a8a1f8bd09a141e52879a03c149fc8a
“henriwf” <henriwolfgangf@gmail.com>
2024-11-13 14:48:35 -0300

    -

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let numAtual = num1;
+    let numTotal = num1;
 
-};
+    for (let i = num1; i < num2; i++) {
+
+        numAtual = numAtual + 1;
+        numTotal = numTotal + numAtual;
+    
+        
+    }
+    return numTotal;
+}
+        
+
+    
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
