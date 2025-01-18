376b501eb0ef07f307b15dc86f06aac49149bde8
henriwf <henriwolfgangf@gmail.com>
2025-01-18 01:55:47 +0000

    solve exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+// finished
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+// finished
+
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+// finished
+
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
+};
+// finished
+
+const power = function(num1, num2) {
+	return num1 ** num2;
+};
+// finished
+
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
-const subtract = function() {
-	
-};
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,4 @@
-const palindromes = function (word) {
-
-const splitword = word.split("");
-const removedSpChar = splitword
-.filter ((letter) letter => letter !== "." || letter !== "!" || letter !== "?");
-.reverse();
-const comparison = removedSpChar.join();
-
-if (word == comparison) {
-    return true
-}
+const palindromes = function () {
 
 };
 
