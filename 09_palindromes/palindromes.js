02068f95cbbe5f5048c5e716b94ef3b7c618e459
henriwf <henriwolfgangf@gmail.com>
2025-01-18 00:00:31 -0300

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function (word) {
-
-    const splitword = word.split("");
-    const invertedWord = splitword.filter ((letter) => letter !== "." && letter !== "!" && letter !== "?").reverse().join("").toLowerCase();
     
-    return invertedWord == word;
+    const splitword = word.split("");
+    
+    const cleanWord = splitword.filter ((letter) => letter !== "." && letter !== " " && letter !== "!" && letter !== "?").join("").toLowerCase();
+    const invertedWord = splitword.filter ((letter) => letter !== "." && letter !== " " && letter !== "!" && letter !== "?").reverse().join("").toLowerCase();
+    
+    return invertedWord == cleanWord;
     
     };
 
