0c52eaaa14c892787fa0acb976a5dec9576956c1
henriwf <henriwolfgangf@gmail.com>
2025-01-17 23:35:02 -0300

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (word) {
 
     const splitword = word.split("");
     const removedSpChar = splitword.filter ((letter) => letter !== "." && letter !== "!" && letter !== "?").reverse();
-    const comparison = removedSpChar.join();
+    const comparison = removedSpChar.join("");
     
     if (word == comparison) {
         return true
