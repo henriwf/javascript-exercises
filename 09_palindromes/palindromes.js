6c0b260cf87e8f3ad33bb77c79cb216b53ec1b22
henriwf <henriwolfgangf@gmail.com>
2025-01-18 01:59:37 +0000

    const palindromes = function (word) {
    const splitword = word.split("");
    const removedSpChar = splitword.filter ((letter) => letter !== "." || letter !== "!" || letter !== "?").reverse();
    const comparison = removedSpChar.join();
    
    if (word == comparison) {
        return true
    }
    
    };

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
-};
+    const splitword = word.split("");
+    const removedSpChar = splitword.filter ((letter) => letter !== "." || letter !== "!" || letter !== "?").reverse();
+    const comparison = removedSpChar.join();
+    
+    if (word == comparison) {
+        return true
+    }
+    
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
