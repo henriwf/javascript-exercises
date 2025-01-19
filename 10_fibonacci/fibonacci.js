12e6abfdd02fa5ed2cdc92a6f9b6554cb89d66b8
henriwf <henriwolfgangf@gmail.com>
2025-01-19 00:55:42 +0000

    solve exercise 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,13 @@
 const fibonacci = function(num) {
+
 let convNum = num.toString();
 let sequence = [];
-
 let prevNum = 0;
 let curNum = 1;
 sequence.push(curNum);
 
-if (convNum == 0) {
-    return 0;
-} else if (convNum < 0) {
-  return "OOPS";
-}
+if (convNum == 0) return 0;
+if (convNum < 0) return "OOPS";
 
 
 for (let i = 1; i < convNum; i++) {
