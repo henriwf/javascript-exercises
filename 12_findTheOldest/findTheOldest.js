dae5078ba782d735b19cd22b4a6754b79551867f
henriwf <henriwolfgangf@gmail.com>
2025-01-19 01:35:38 +0000

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  
+    const updatedArray = array.map(person => ({
+        ...person,
+        age: person.yearOfDeath 
+          ? person.yearOfDeath - person.yearOfBirth 
+          : new Date().getFullYear() - person.yearOfBirth
+      }));
+
+      const orderedArray = updatedArray.sort((a, b) => a.age > b.age ? 1 : -1);
+      
+      return orderedArray[orderedArray.length-1];
+    
+
 
 };
 
